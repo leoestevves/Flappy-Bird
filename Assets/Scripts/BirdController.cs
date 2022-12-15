4a4132f1eb68795261fd76e539726506483c67d7
Leonardo Esteves <61708681+leoestevves@users.noreply.github.com>
2022-12-15 16:32:15 -0300

    Criando Pipes, melhorando pulo

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -5,28 +5,27 @@ using UnityEngine.UI;
 
 public class BirdController : MonoBehaviour
 {
-    public Rigidbody2D birdRigidbody;
+    private Rigidbody2D birdRigidbody;
     public int jumpForce;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-
+        birdRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump")) //Jump definido no project settings;
         {
             Jump();
         }
         
     }
-
+        
     void Jump()
     {
-        birdRigidbody.velocity = Vector2.up * jumpForce;
+        birdRigidbody.velocity = Vector2.up * jumpForce; 
     }
 }
